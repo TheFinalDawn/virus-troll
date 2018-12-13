db76d041cd340995f6238d93f079f91b9f6e7416
TheFinalDawn <45810243+TheFinalDawn@users.noreply.github.com>
2018-12-13 14:06:06 -0800

    Add you-win textfile
    This is a you win prank

---
+++ you-win.txt.bat
@@ -0,0 +1,176 @@
+@echo off
+title Free Call of Duty!
+color B
+echo Congratulations! We have randomly selected and YOU HAVE WON!
+ping n- 2 127.0.0.1>nul
+echo "What did I win?" you may ask...
+ping n- 2 127.0.0.1>nul
+echo Well, you have won a FREE Fortnite Save The World download!
+ping n- 2 127.0.0.1>nul
+echo Would you like to download now?!
+echo Type Yes or No
+set input=
+set /p input=
+if %input%== Yes goto 1
+if %input%== No goto 2
+:1
+Cls
+color A
+echo Thank you for downloading.
+ping n- 2 127.0.0.1>nul
+echo Downloading... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 8%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 14%
+ping n- 2 127.0.0.1>nul
+echo Downloading... 16%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Error.exe.
+ping n- 2 127.0.0.1>nul
+echo Download has been paused.
+pause
+echo A virus has been detected. Do you want to delete it?
+echo Type 1 to delete
+set input=
+set /p input=
+if %input%== 1 goto 4
+:2
+Cls
+echo Are you sure you don't want to download a free game?
+echo Type 2 for Yes or type 3 for No
+set input=
+set /p input=
+if %input%== 2 goto 1
+if %input%== 3 goto 3
+:3
+Cls
+color C
+echo Oh well. We're not letting you throw away a perfect
+echo video game that's FREE.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+goto 1
+:4
+Cls
+color C
+echo Deleting virus... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Deleting virus... 37%
+echo Deleting virus... 43%
+ping n- 2 127.0.0.1>nul
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo Failure to delete virus.
+echo =!WARNING!=
+echo Your data is at risk!
+pause
+echo Deleting all SYSTEM32 files...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+echo Cleared.
+pause
+Cls
+color 4
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files being restored.
+pause
+echo Error.exe
+echo SYSTEM32 files breached.
+ping n- 2 127.0.0.1>nul
+echo !@#.4fa
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo Hehehe...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo I'm in control now...
+ping n- 2 127.0.0.1>nul
+echo I have your SYSTEM32 files now...
+ping n- 2 127.0.0.1>nul
+echo In case you didn't know,
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files are what run your computer.
+ping n- 2 127.0.0.1>nul
+echo You'll be happy that it's under my control.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color C
+echo ERROR ERROR ERROR
+ping n- 2 127.0.0.1>nul
+echo Hack.exe Successful.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo Hey... It's me again.
+ping n- 2 127.0.0.1>nul
+echo You scared yet? Not sure what to do?
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Because...
+ping n- 2 127.0.0.1>nul
+Cls
+color F
+echo YOU SHOULD HAVE SEEN THE LOOK ON YOUR FACE!!!
+ping n- 2 127.0.0.1>nul
+echo I TOTALLY GOT YOU!!!
+ping n- 2 127.0.0.1>nul
+echo ALL I DID WAS PUT SOME CODING INTO NOTEPAD AND MAKE IT LOOK LIKE YOU GOT HACKED!
+ping n- 2 127.0.0.1>nul
+echo Oh, I am just DYING here right now!
+ping n- 2 127.0.0.1>nul
+echo Are you crying?
+ping n- 2 127.0.0.1>nul
+echo Because I am.
+echo DUE TO ALL THIS LAUGHTER!!!
+ping n- 2 127.0.0.1>nul
+echo LOLOLOLOLOLOLOLOLOLOLOLOLOL!!!!!
+ping n- 2 127.0.0.1>nul
+echo #TROLLS FOR DAYS!!!
+ping n- 2 127.0.0.1>nul
+echo Ahhhhh... That was fun.
+ping n- 2 127.0.0.1>nul
+echo I want to let you know that nothing happened.
+ping n- 2 127.0.0.1>nul
+echo Nothing was deleted.
+echo The only thing I did was make this Batch File say stuff to make it look like you got hacked.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo If you think back to it, don't you find it just a LITTLE funny?
+ping n- 2 127.0.0.1>nul
+echo Yeah, this took me a while to make.
+ping n- 2 127.0.0.1>nul
+echo It was fun doing this. It really was.
+ping n- 2 127.0.0.1>nul
+echo I hope to see you around some time...
+ping n- 2 127.0.0.1>nul
+echo And if I do, I would really like to teach you coding and such.
+echo Like how I made this fake virus delio.
+echo I made it in Notepad if that helps your case.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Later!
+ping n- 2 127.0.0.1>nul
+echo Press any button to exit after you're done reading.
+ping n- 2 127.0.0.1>nul
+pause
+exit
