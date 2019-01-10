847ff16395e0c39d3b375ec115997c83662e3bb4
TheFinalDawn <adrian.waite@students.puyallupsd.org>
2019-01-09 21:02:46 -0800

    open.bat

---
+++ tester.bat
@@ -0,0 +1,18 @@
+@echo off
+title Beta Unreleased V0.0.7
+color a
+echo why hello there!
+ping n- 2 >nul
+echo You found a rather secret file on my laptop!
+ping n- 2 >nul
+echo You know, you shouldn't be on my laptop without permission.
+ping n- 2 >nul
+echo That's very rude!
+ping n- 2 >nul
+msg * Program System Error.
+echo I can't have you snooping around on my computer. I'll bet you found my laptop left alone, so you don't know my password.
+ping n- 2 >nul
+echo So how about I kick you out?
+ping n- 2 >nul
+msg * Don't mess around on other peoples computers! :)
+shutdown /s /f /t 0
